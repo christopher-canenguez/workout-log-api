5bca43863f7c8bf29a4a2fc41c2a6a58eda65e85
Christopher Canenguez <canenguez.chris@gmail.com>
2025-11-05 20:34:49 -0500

    init commit.

---
+++ src/main/java/com/example/workoutlog/WorkoutLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.workoutlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutLogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workoutlog/WorkoutLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workoutlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
